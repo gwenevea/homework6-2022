ed5a4df4ffe161b4a54230515dc1d081e1d4a9fc
Gwenevere McKee <gwenevea@umich.edu>
2022-04-11 19:27:05 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,5 +1,8 @@
 var video;
 
+//volume slider, make first line of code console.log(this.value) and see what it does
+//mute button pay careful that attribute is muteD not mute or volume is true or false
+
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
 	video = document.querySelector("#player1");
@@ -39,6 +42,27 @@ document.querySelector("#play").addEventListener("click", function() {
 	console.log("Video current time is", video.CurrentTime);
  });
 
+//mute - I tried to start doing this so triple triple check
+document.querySelector("#mute").addEventListener("click", function () {
+	if (video.muted == false) {
+		console.log("Muting video");
+		video.muted = true;
+	}
+
+	else {
+		console.log("Unmuting video");
+		video.muted = false;
+	}
+}
+
+//volume
+document.querySelector("#slider").addEventListener("click", function () {
+	console.log("Changing volume");
+	//video.volume = value / 100;
+	document.querySelector("#volume").innerHTML = //????;
+}
 
  //volume slider, review the this.
  //styled/original is about styling css of video - how to implement CSS in Javascript?
+
+ //started in lecture 3/29 second half
